dea332975f36070e01e1687564366d2bcf417739
irfanyazid-13032000 <irfanyazid18@gmail.com>
2024-02-05 22:11:43 +0700

    membuat wishlist

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { WishItem } from '../shared/models/wishItem';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +10,20 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  items : WishItem[] = [
+    new WishItem("learn japanese",false),
+    new WishItem("learn korean"),
+    new WishItem("learn mandarin",true)
+  ]
   title = 'wishlist';
+  toggleItem(e : any){
+    e.isComplete = !e.isComplete
+    console.log(e)
+  }
+
+  newWishText = ""
+
+  addNewWish(){
+
+  }
 }
---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { BrowserModule } from "@angular/platform-browser";
+
+// import { AppComponent } from "./app.component";
+
+@NgModule({
+  declarations:[
+    
+  ],
+  imports:[
+    BrowserModule,
+    FormsModule
+  ],
+  providers:[],
+  bootstrap:[]
+})
+
+export class AppModule { }
